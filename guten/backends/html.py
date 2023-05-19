840d7146c38cd2875072ef0cf760fc87c332be47
Elton <eltonp3103@gmail.com>
2023-05-18 21:08:31 -0400

    Refactor filtering and metadata handling into separate modules

--- guten/backends/html.py
+++ guten/backends/html.py
@@ -1,14 +1,13 @@
 from typing import List
 from pathlib import Path
-from datetime import datetime, timedelta, timezone
-from dateutil.parser import parse as date_parse
+from datetime import datetime
 
 from guten.backend import Backend
 from guten.press import FetchedSourceGroup
 from guten.utils import eprint
+from guten.filters import DefaultFilter
+from guten.metadata import L10MetadataScheme
 
-
-METADATA_FILE = ".metadata"
 
 def begin_document():
     return "<html>"
@@ -84,103 +83,43 @@ def source_item(data):
         </details>
     """
 
-def DEFAULT_HIST(today):
-    return today - timedelta(days=5)
-
-
-def parse_metadata(data, today):
-    dates = [date_parse(d.strip()) for d in data.split(";")]
-
-    if len(dates) == 0:
-        return DEFAULT_HIST(today), DEFAULT_HIST(today)
-    elif len(dates) == 1:
-        return dates[0], DEFAULT_HIST(today)
-    else:
-        return dates[0], dates[1]
-    
 
 class HTMLBackend(Backend):
     async def run(self, groups: List[FetchedSourceGroup], output_dir: Path) -> Path:
-        today = datetime.now(timezone.utc)
-        output_file = output_dir / f"{today.year}-{today.month}-{today.day}.html"
-        metadata_file = output_dir / METADATA_FILE
+        with L10MetadataScheme(output_dir) as metadata:
+            today = metadata.today
+            output_file = output_dir / f"{today.year}-{today.month}-{today.day}.html"
+            previous_run_date = metadata.previous_run_date
+            groups = DefaultFilter().apply(groups, previous_run_date)
 
-        hist0, hist1 = None, None
-        if metadata_file.exists():
-            with open(metadata_file, "r") as f:
-                data = f.read()
-                hist0, hist1 = parse_metadata(data, today)
-        else:
-            hist0, hist1 = DEFAULT_HIST(today), DEFAULT_HIST(today)
+            eprint(f"Showing entries published after {previous_run_date}")
 
-        # Now, hist0 and hist1 have been set
+            with open(output_file, "w") as f:
+                f.write(begin_document())
+                f.write(preamble())
 
-        # If guten is run more than once, we use hist1 as the previous run
-        # date. This approximates a fresh re-run of guten for that day.
-        if hist0.date() == today.date():
-            previous_run_date = hist1
-        else:
-            previous_run_date = hist0
+                f.write(begin_body())
 
-        eprint(f"Showing entries published after {previous_run_date}")
+                localdate = datetime.now()
+                f.write(h1(f"{localdate.year}/{localdate.month}/{localdate.day}"))
 
-        with open(output_file, "w") as f:
-            f.write(begin_document())
-            f.write(preamble())
+                for (group, sources) in groups:
+                    f.write(h2(group.name.title()))
+                    for source, data in sources:
+                        data = data.apply(lambda x: source_item(x), axis=1)
+                        data = list(data)
 
-            f.write(begin_body())
+                        f.write(h3(source.name))
+                        # f.write(begin_list())
+                        f.write("".join(data))
+                        # f.write(end_list())
 
-            localdate = datetime.now()
-            f.write(h1(f"{localdate.year}/{localdate.month}/{localdate.day}"))
+                f.write(small(f"Generated at: {today.isoformat()}"))
 
-            def process_source(item):
-                source, df = item
-                if df.empty:
-                    return source, []
-                data = df
-                if "published" in df:
-                    df["date"] = df["published"].apply(lambda x: date_parse(x))
-                    data = df[df["date"] > previous_run_date]
-                data = data[["title", "link", "summary"]]
-                data = data.apply(lambda x: source_item(x), axis=1)
-                data = list(data)
-                return source, data
-
-
-            for (group, sources) in groups:
-                sources = [process_source(source) for source in sources]
-                sources = [(source, data) for (source, data) in sources if len(data) > 0]
-                if len(sources) == 0:
-                    continue
-
-                f.write(h2(group.name.title()))
-                for source, data in sources:
-                    f.write(h3(source.name))
-                    # f.write(begin_list())
-                    f.write("".join(data))
-                    # f.write(end_list())
-
-            f.write(small(f"Generated at: {today.isoformat()}"))
-
-            f.write(end_body())
-            f.write(end_document())
-
-        with open(metadata_file, "w") as f:
-            # If hist0.date() == today.date(), set metadate file to:
-            # <hist0>;<hist1>
-            # Otherwise, set metadata file to:
-            # <today>;<hist0>
-            if hist0.date() == today.date():
-                f.write(hist0.isoformat())
-                f.write(";")
-                f.write(hist1.isoformat())
-            else:
-                f.write(today.isoformat())
-                f.write(";")
-                f.write(hist0.isoformat())
+                f.write(end_body())
+                f.write(end_document())
 
         return output_file
-
 
 
 __backend__ = HTMLBackend
